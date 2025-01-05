3a8ca52a57800b9de1306751a8e81a5e9bcec2e1
ManojaD2004 <manojadkc2004@gmail.com>
2025-01-05 20:56:48 +0530

    Changes

---
+++ python/commands.sh
@@ -0,0 +1,8 @@
+# Create a Virtual Env
+python3 -m venv deps
+
+# Activate it
+source ./deps/bin/activate
+
+# Install deps
+pip install -r requirements.txt
--- python/main.py
+++ python/main.py
@@ -1,9 +1,13 @@
 from ultralytics import YOLO
+from PIL import Image
 
 model = YOLO("yolo11n.pt")
 
-results = model("./bus.jpg")
+# results = model("./bus.jpg")
+
+im1 = Image.open("./bus.jpg")
+results = model.predict(source=im1, save=True)  
 
 # Visualize the results
-for result in results:
-    result.show()
+# for result in results:
+#     result.show()
