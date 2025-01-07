ffa8eedfc25b5e04c060021284b9f0a1dfc065b9
ManojaD2004 <manojadkc2004@gmail.com>
2025-01-07 15:42:38 +0530

    Nice

--- commands.sh
+++ commands.sh
@@ -1,3 +1,3 @@
 docker build -t techgium-server ./
 
-docker run -it --rm -p 9000:9000 -v ./images:/app/images -v ./runs:/app/runs  techgium-server
+docker run -it --rm -p 9000:9000 -v ./images:/app/images -v ./runs:/app/server/runs  techgium-server
