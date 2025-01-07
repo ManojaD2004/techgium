e840ed6256a6739cea271caff3fd7182f8503984
ManojaD2004 <manojadkc2004@gmail.com>
2025-01-07 15:50:53 +0530

    Nice

--- client/app/component/Test.js
+++ client/app/component/Test.js
@@ -1,9 +1,10 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const VideoFeed = () => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
+  const [turnOnOff, setTurnOnOff] = useState(false);
 
   const captureAndSendFrame = async () => {
     const video = videoRef.current;
@@ -39,20 +40,12 @@ const VideoFeed = () => {
     }
   };
 
-  const startVideoStream = async () => {
-    try {
-      const stream = await window.navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true,
-      });
-      videoRef.current.srcObject = stream;
-    } catch (error) {
-      console.error("Error accessing webcam:", error);
-    }
-  };
-
   useEffect(() => {
     if (!window) {
+      return;
+    }
+    if (!turnOnOff) {
+      videoRef.current.srcObject = null;
       return;
     }
     const startVideoStream = async () => {
@@ -72,10 +65,10 @@ const VideoFeed = () => {
     const intervalId = setInterval(captureAndSendFrame, 5000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [turnOnOff]);
 
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen flex-col">
       <video
         ref={videoRef}
         autoPlay
@@ -86,7 +79,7 @@ const VideoFeed = () => {
           canvasRef.current.height = video.videoHeight;
         }}
       />
-      {/* <button onClick={startVideoStream}>Click me</button> */}
+      <button className="block" onClick={() => setTurnOnOff(!turnOnOff)}>Turn On/Off</button>
       <canvas ref={canvasRef} style={{ display: "none" }} />
     </div>
   );
